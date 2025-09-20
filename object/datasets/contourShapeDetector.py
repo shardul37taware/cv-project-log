a57776ace2a29b549bf32908ad994f48128d6fd9
shardul37taware <shardul.santosh.taware@gmail.com>
2025-09-20 12:46:59 +0530

    .

---
+++ object/datasets/contourShapeDetector.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+def empty(a):
+    pass
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Threshold 1", "Parameters", 150, 255, empty)
+cv2.createTrackbar("Threshold 2", "Parameters", 150, 255, empty)
+
+
+while True:
+    ret, frame = cap.read()
+
+    imgBlur = cv2.GaussianBlur(frame, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    threshold1 = cv2.getTrackbarPos("Threshold 1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold 2", "Parameters")
+
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+
+
+    cv2.imshow("Shape Detector", imgCanny)
+    if cv2.waitKey(1) & 0xff == ord(' '):
+        break
